9584131c30b561efa9029aaacb4bdd6548d1cd33
Iury Vasconcelos <iury0393@gmail.com>
2020-11-09 16:47:23 -0300

    refatoração do feed

--- lib/screens/home/feed/feed_screen.dart
+++ lib/screens/home/feed/feed_screen.dart
@@ -28,23 +28,15 @@ class _FeedScreenState extends State<FeedScreen> {
   @override
   void initState() {
     super.initState();
-    _bloc.getUser().then((value) {
-      setState(() {
-        _bloc.userEmail = value.data.email;
-        _bloc.user = value.data;
-      });
-    });
-    _bloc.doRequestGetFeed(true);
-    EventCenter.getInstance().newPostEvent.subscribe(_refreshFeed);
-    EventCenter.getInstance().deletePostEvent.subscribe(_refreshFeedDelete);
+    _blocInit();
+    _eventsSubscribe();
   }
 
   @override
   void dispose() {
-    super.dispose();
     _bloc.dispose();
-    EventCenter.getInstance().newPostEvent.unsubscribe(_refreshFeed);
-    EventCenter.getInstance().deletePostEvent.unsubscribe(_refreshFeedDelete);
+    super.dispose();
+    _eventsUnsubscribe();
   }
 
   @override
@@ -55,90 +47,41 @@ class _FeedScreenState extends State<FeedScreen> {
           titleImg: 'assets/navlogo.png',
           center: true,
           widgets: [
-            FlatButton(
-              onPressed: () {
-                _doNavigateToPostScreen();
-              },
-              child: Icon(
-                Icons.add_box_outlined,
-                color: kSkillaPurple,
-              ),
-            ),
+            _buildFlatBtn(),
           ],
         ),
         body: Padding(
           padding: Utils.getPaddingDefault(),
-          child: StreamBuilder<BaseResponse<List<Post>>>(
-            stream: _bloc.feedController.stream,
-            builder: (context, snapshot) {
-              switch (snapshot.data?.status) {
-                case Status.LOADING:
-                  return Center(
-                    child: NativeLoading(animating: true),
-                  );
-                  break;
-                case Status.ERROR:
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    NativeDialog.showErrorDialog(
-                        context, snapshot.data.message);
-                  });
-                  return Container();
-                  break;
-                default:
-                  if (snapshot.data != null) {
-                    if (snapshot.data.data.isNotEmpty) {
-                      return SmartRefresher(
-                        controller: _bloc.refreshController,
-                        enablePullUp: false,
-                        enablePullDown: true,
-                        child: _buildListFeed(snapshot),
-                        physics: BouncingScrollPhysics(),
-                        header: ClassicHeader(
-                          refreshingIcon: NativeLoading(animating: true),
-                          refreshingText: AppLocalizations.of(context)
-                              .translate('textLoading'),
-                          completeText: AppLocalizations.of(context)
-                              .translate('textRefreshCompleted'),
-                          idleText: AppLocalizations.of(context)
-                              .translate('textPullToRefresh'),
-                          releaseText: AppLocalizations.of(context)
-                              .translate('textReleaseToRefresh'),
-                          completeIcon: Icon(
-                            Icons.check,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        onRefresh: _onRefresh,
-                      );
-                    } else {
-                      return Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          AppLocalizations.of(context)
-                              .translate('textFeedWarning'),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyles.paragraph(
-                            TextSize.xLarge,
-                            weight: FontWeight.w400,
-                            color: kSkillaPurple,
-                          ),
-                        ),
-                      );
-                    }
-                  }
-                  return Container();
-              }
-            },
+          child: _StreamBuilderFeed(
+            bloc: _bloc,
           ),
         ),
       ),
     );
   }
 
-  _onRefresh() async {
-    await _bloc.refreshFeed();
-    _bloc.refreshController.refreshCompleted();
+  // >>>>>>>>>> INIT
+
+  _blocInit() {
+    _bloc.getUser().then((value) {
+      setState(() {
+        _bloc.userEmail = value.data.email;
+        _bloc.user = value.data;
+      });
+    });
+    _bloc.doRequestGetFeed(true);
+  }
+
+  // >>>>>>>>>> EVENTS
+
+  _eventsSubscribe() {
+    EventCenter.getInstance().newPostEvent.subscribe(_refreshFeed);
+    EventCenter.getInstance().deletePostEvent.subscribe(_refreshFeedDelete);
+  }
+
+  _eventsUnsubscribe() {
+    EventCenter.getInstance().newPostEvent.unsubscribe(_refreshFeed);
+    EventCenter.getInstance().deletePostEvent.unsubscribe(_refreshFeedDelete);
   }
 
   _refreshFeed(NewPostEventArgs args) {
@@ -153,26 +96,159 @@ class _FeedScreenState extends State<FeedScreen> {
     }
   }
 
-  ListView _buildListFeed(AsyncSnapshot<BaseResponse<List<Post>>> snapshot) {
-    return ListView.builder(
-      itemCount: snapshot.data.data.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 25.0),
-          child: PostItem(
-            post: snapshot.data.data.elementAt(index),
-            user: _bloc.user,
-          ),
-        );
+  // >>>>>>>>>> WIDGETS
+
+  Widget _buildFlatBtn() {
+    return FlatButton(
+      onPressed: () {
+        _doNavigateToPostScreen();
       },
+      child: Icon(
+        Icons.add_box_outlined,
+        color: kSkillaPurple,
+      ),
     );
   }
+
+  // >>>>>>>>>> NAVIGATORS
 
   _doNavigateToPostScreen() {
     Navigator.of(context).push(
       CupertinoPageRoute(
         builder: (context) => PostScreen(),
       ),
+    );
+  }
+}
+
+class _StreamBuilderFeed extends StatefulWidget {
+  final FeedBloc bloc;
+
+  _StreamBuilderFeed({this.bloc});
+  @override
+  __StreamBuilderFeedState createState() => __StreamBuilderFeedState();
+}
+
+class __StreamBuilderFeedState extends State<_StreamBuilderFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<BaseResponse<List<Post>>>(
+      stream: widget.bloc.feedController.stream,
+      builder: (context, snapshot) {
+        switch (snapshot.data?.status) {
+          case Status.LOADING:
+            return Center(
+              child: NativeLoading(animating: true),
+            );
+            break;
+          case Status.ERROR:
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              NativeDialog.showErrorDialog(context, snapshot.data.message);
+            });
+            return Container();
+            break;
+          default:
+            if (snapshot.data != null) {
+              if (snapshot.data.data.isNotEmpty) {
+                return _SmartRefresher(
+                  bloc: widget.bloc,
+                  snapshot: snapshot,
+                );
+              } else {
+                return Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    AppLocalizations.of(context).translate('textFeedWarning'),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyles.paragraph(
+                      TextSize.xLarge,
+                      weight: FontWeight.w400,
+                      color: kSkillaPurple,
+                    ),
+                  ),
+                );
+              }
+            }
+            return Container();
+        }
+      },
+    );
+  }
+}
+
+class _SmartRefresher extends StatefulWidget {
+  final FeedBloc bloc;
+  final AsyncSnapshot<BaseResponse<List<Post>>> snapshot;
+
+  _SmartRefresher({this.bloc, this.snapshot});
+
+  @override
+  __SmartRefresherState createState() => __SmartRefresherState();
+}
+
+class __SmartRefresherState extends State<_SmartRefresher> {
+  @override
+  Widget build(BuildContext context) {
+    return SmartRefresher(
+      controller: widget.bloc.refreshController,
+      enablePullUp: false,
+      enablePullDown: true,
+      child: _BuildListFeed(
+        bloc: widget.bloc,
+        snapshot: widget.snapshot,
+      ),
+      physics: BouncingScrollPhysics(),
+      header: _classicHeader(),
+      onRefresh: _onRefresh,
+    );
+  }
+
+  Widget _classicHeader() {
+    return ClassicHeader(
+      refreshingIcon: NativeLoading(animating: true),
+      refreshingText: AppLocalizations.of(context).translate('textLoading'),
+      completeText:
+          AppLocalizations.of(context).translate('textRefreshCompleted'),
+      idleText: AppLocalizations.of(context).translate('textPullToRefresh'),
+      releaseText:
+          AppLocalizations.of(context).translate('textReleaseToRefresh'),
+      completeIcon: Icon(
+        Icons.check,
+        color: Colors.grey,
+      ),
+    );
+  }
+
+  _onRefresh() async {
+    await widget.bloc.refreshFeed();
+    widget.bloc.refreshController.refreshCompleted();
+  }
+}
+
+class _BuildListFeed extends StatefulWidget {
+  final FeedBloc bloc;
+  final AsyncSnapshot<BaseResponse<List<Post>>> snapshot;
+
+  _BuildListFeed({this.bloc, this.snapshot});
+  @override
+  __BuildListFeedState createState() => __BuildListFeedState();
+}
+
+class __BuildListFeedState extends State<_BuildListFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.snapshot.data.data.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 25.0),
+          child: PostItem(
+            post: widget.snapshot.data.data.elementAt(index),
+            user: widget.bloc.user,
+          ),
+        );
+      },
     );
   }
 }
